7cd6235de9ef951a4d90f36cce21c0301a23f23f
ej070961 <ej070961@gmail.com>
2024-09-13 21:48:10 +0900

    feat: 카카오맵 API 연결 및 맵 공통 컴포넌트 구현

---
+++ src/components/Common/MapContainer.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Map } from "react-kakao-maps-sdk";
+import { getLocation } from "../../hooks/getLocation";
+
+type MapContainerProps = {
+  lat?: number;
+  lng?: number;
+  children?: React.ReactNode;
+};
+
+function MapContainer({ lat, lng, children }: MapContainerProps) {
+  const [geo, setGeo] = useState({ lat: 0, lng: 0 });
+
+  useEffect(() => {
+    //현 위치 받아오기
+    const fetchLocation = async () => {
+      if (lat && lng) {
+        // lat과 lng이 props로 전달되면 해당 값 사용
+        setGeo({ lat, lng });
+      } else {
+        // 전달되지 않으면 현재 위치 가져오기
+        const res = await getLocation();
+        if (res) {
+          setGeo(res);
+        }
+      }
+    };
+    console.log(geo);
+
+    fetchLocation();
+  }, [lat, lng]);
+
+  return (
+    <Map
+      id="map"
+      level={3}
+      center={{ lat: geo.lat, lng: geo.lng }}
+      style={{ width: "100%", height: "calc(100vh - 60px)" }}
+    >
+      {children}
+    </Map>
+  );
+}
+
+export default MapContainer;
