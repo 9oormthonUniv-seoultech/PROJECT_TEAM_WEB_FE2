279ce59ea2b177c4dc151f788ad1ca49b1e97889
ej070961 <ej070961@gmail.com>
2024-09-17 22:50:08 +0900

    feat: 카카오맵 커스텀 마커 생성

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,16 @@
 import tw from "twin.macro";
 import styled from "styled-components";
 import NavBar from "../../components/Common/NavBar";
-import MapContainer from "../../components/Common/MapContainer";
 import Search from "../../components/Home/Search";
 import AddBtn from "../../components/Home/AddBtn";
 import BoothSlide from "../../components/Home/BoothSlide";
+import BoothMap from "../../components/Home/BoothMap";
 
 function Home() {
   return (
     <Layout>
       <main className="w-full relative">
-        <MapContainer></MapContainer>
+        <BoothMap />
         <Search />
         <BoothSlide />
         <AddBtn />
