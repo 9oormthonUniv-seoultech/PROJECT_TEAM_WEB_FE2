f3dbc3484246f9dc8d7122c7096ac0ed9e94717b
jaeyo03 <toyazx@naver.com>
2024-09-18 22:44:48 +0900

    refactor : 인덴트 수정

--- src/pages/PhotoUpload/index.tsx
+++ src/pages/PhotoUpload/index.tsx
@@ -11,16 +11,16 @@ interface OptionProps {
 
 const OptionComponent = ({ onClick, isActive, label, subLabel }: OptionProps) => {
   return (
-      <Option onClick={onClick} isActive={isActive} label={label} subLabel={subLabel}>
-        <div css={tw`flex items-center gap-3`}>
-          <CircleContainer>
-            <CircleBorder isActive={isActive} />
-            {isActive && <CircleInner />}
-          </CircleContainer>
-          <Label isActive={isActive}>{label}</Label>
-        </div>
-        {isActive && subLabel && <SubLabel>{subLabel}</SubLabel>}
-      </Option>
+    <Option onClick={onClick} isActive={isActive} label={label} subLabel={subLabel}>
+      <div css={tw`flex items-center gap-3`}>
+        <CircleContainer>
+          <CircleBorder isActive={isActive} />
+          {isActive && <CircleInner />}
+        </CircleContainer>
+        <Label isActive={isActive}>{label}</Label>
+      </div>
+      {isActive && subLabel && <SubLabel>{subLabel}</SubLabel>}
+    </Option>
   );
 };
 
@@ -37,53 +37,53 @@ function PhotoUpload() {
   };
 
   return (
-      <Container>
-        <div className="w-[390px] h-[146px] relative bg-white mb-12">
-          <div className="left-[150px] top-[82px] absolute text-[#171d24] text-2xl font-semibold font-['Pretendard']">
-            사진 등록
-          </div>
-          <div className="w-6 h-6 p-0.5 left-[345px] top-[85px] absolute justify-center items-center gap-2.5 inline-flex">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g id="Frame">
-                <path
-                    id="Vector"
-                    d="M2.5 2.5L21.5 21.5"
-                    stroke="#676F7B"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                />
-                <path
-                    id="Vector_2"
-                    d="M21.5 2.5L2.5 21.5"
-                    stroke="#676F7B"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                />
-              </g>
-            </svg>
-          </div>
+    <Container>
+      <div className="w-[390px] h-[146px] relative bg-white mb-12">
+        <div className="left-[150px] top-[82px] absolute text-[#171d24] text-2xl font-semibold font-['Pretendard']">
+          사진 등록
         </div>
-        <OptionContainer>
-          <OptionComponent
-              onClick={() => handleOptionClick("QR 인식")}
-              isActive={activeOption === "QR 인식"}
-              label="QR 인식"
-              subLabel="QR 인식은 하루필름 매장만 가능해요"
-          />
-          <OptionComponent
-              onClick={() => handleOptionClick("내 사진첩 불러오기")}
-              isActive={activeOption === "내 사진첩 불러오기"}
-              label="내 사진첩 불러오기"
-          />
-        </OptionContainer>
-        <ButtonContainer onClick={() => handleNext()}>
-          <button className="text-center text-white text-[22px] font-semibold font-['Pretendard']">
-            다음
-          </button>
-        </ButtonContainer>
-      </Container>
+        <div className="w-6 h-6 p-0.5 left-[345px] top-[85px] absolute justify-center items-center gap-2.5 inline-flex">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="Frame">
+              <path
+                  id="Vector"
+                  d="M2.5 2.5L21.5 21.5"
+                  stroke="#676F7B"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+              />
+              <path
+                  id="Vector_2"
+                  d="M21.5 2.5L2.5 21.5"
+                  stroke="#676F7B"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+      <OptionContainer>
+        <OptionComponent
+            onClick={() => handleOptionClick("QR 인식")}
+            isActive={activeOption === "QR 인식"}
+            label="QR 인식"
+            subLabel="QR 인식은 하루필름 매장만 가능해요"
+        />
+        <OptionComponent
+            onClick={() => handleOptionClick("내 사진첩 불러오기")}
+            isActive={activeOption === "내 사진첩 불러오기"}
+            label="내 사진첩 불러오기"
+        />
+      </OptionContainer>
+      <ButtonContainer onClick={() => handleNext()}>
+        <button className="text-center text-white text-[22px] font-semibold font-['Pretendard']">
+          다음
+        </button>
+      </ButtonContainer>
+    </Container>
   );
 }
 
