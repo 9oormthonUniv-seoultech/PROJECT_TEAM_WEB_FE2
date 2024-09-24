75e9e16fb049bbb24bd45b8ac5e40389a3770484
ej070961 <ej070961@gmail.com>
2024-09-24 11:29:17 +0900

    feat: 포토부스 커스텀 마커 생성 및 현위치 마커 구현

--- src/components/Common/MapContainer.tsx
+++ src/components/Common/MapContainer.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { Map } from "react-kakao-maps-sdk";
-import { getCurrentLocation } from "../../hooks/getLocation";
-
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { getCurrentLocation, trackCurrentPosition } from "../../hooks/getLocation";
+import PointSVG from "../../assets/images/point.svg?url";
+import HeadingSVG from "../../assets/images/heading.svg?url";
 type MapContainerProps = {
   lat?: number;
   lng?: number;
@@ -10,6 +11,20 @@ type MapContainerProps = {
 
 function MapContainer({ lat, lng, children }: MapContainerProps) {
   const [geo, setGeo] = useState({ lat: 0, lng: 0 });
+  const [position, setPosition] = useState<{ lat: number; lng: number; heading: number | null }>({
+    lat: 0,
+    lng: 0,
+    heading: null,
+  });
+  useEffect(() => {
+    // 사용자 위치 및 방향을 추적하고 업데이트하는 콜백 함수
+    const stopTracking = trackCurrentPosition(setPosition);
+
+    return () => {
+      // 컴포넌트가 언마운트될 때 위치 추적을 멈춤
+      stopTracking();
+    };
+  }, []);
 
   useEffect(() => {
     //현 위치 받아오기
@@ -36,6 +51,40 @@ function MapContainer({ lat, lng, children }: MapContainerProps) {
       center={{ lat: geo.lat, lng: geo.lng }}
       style={{ width: "100%", height: "calc(100vh - 60px)" }}
     >
+      {/* 사용자 위치를 나타내는 원형 마커 */}
+      <MapMarker
+        position={{ lat: position.lat, lng: position.lng }}
+        image={{
+          src: PointSVG,
+          size: {
+            width: 20,
+            height: 20,
+          },
+        }}
+      />
+      {/* 방향을 나타내는 화살표 마커 */}
+      {position.heading !== null && (
+        <MapMarker
+          position={{ lat: geo.lat, lng: geo.lng }}
+          image={{
+            src: HeadingSVG,
+            size: {
+              width: 40,
+              height: 40,
+            },
+            options: {
+              // 화살표 회전
+              offset: {
+                x: 20,
+                y: 20,
+              },
+            },
+          }}
+          // style={{
+          //   transform: `rotate(${position.heading}deg)`, // heading 값을 사용해 화살표 회전
+          // }}
+        />
+      )}
       {children}
     </Map>
   );
--- src/hooks/getLocation.tsx
+++ src/hooks/getLocation.tsx
@@ -3,7 +3,7 @@
 //     .then((res) => res.json())
 //     .then((res) => res["IPv4"]);
 
-// export const getLocation = async () => {
+// export const getCurrentLocation = async () => {
 //   const nowIp = await getIp();
 //   const geoData = await fetch(`http://ip-api.com/json/${nowIp}`)
 //     .then((res) => res.json())
@@ -22,13 +22,46 @@
 
 export const getCurrentLocation = async () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-
-      return { lat: lat, lng: lon };
+    return new Promise<{ lat: number; lng: number }>((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+          resolve({ lat, lng });
+        },
+        (error) => {
+          reject(error);
+        }
+      );
     });
   }
-  //사용할 수 없을 경우 기본 위치 리턴
+  // 사용자의 위치 정보를 가져올 수 없는 경우, 기본 위치 리턴
   return { lat: 37.63763525003301, lng: 127.07945581420265 };
+};
+
+export const trackCurrentPosition = (
+  onUpdate: (position: { lat: number; lng: number; heading: number | null }) => void
+) => {
+  if (navigator.geolocation) {
+    const watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        const heading = position.coords.heading; // 장치의 방향 정보
+        console.log(lat, lng, heading);
+        // 새로운 위치와 방향을 업데이트하는 콜백 호출
+        onUpdate({ lat, lng, heading });
+      },
+      (error) => {
+        console.error("Geolocation error:", error);
+      },
+      { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+    );
+
+    // 추적을 멈추고 싶을 때 호출할 수 있는 clear 함수 반환
+    return () => navigator.geolocation.clearWatch(watchId);
+  }
+
+  console.error("Geolocation is not supported by this browser.");
+  return () => {};
 };
