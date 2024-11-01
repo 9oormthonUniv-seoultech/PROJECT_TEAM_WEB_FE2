200287c18a83c7436b0e0df693bd51af10e97d45
jaeyo03 <toyazx@naver.com>
2024-11-01 15:05:00 +0900

    feat : 이미지 카드 수정

--- src/components/Album/ImageCard.tsx
+++ src/components/Album/ImageCard.tsx
@@ -5,24 +5,34 @@ import LikeFilledIcon from "../../assets/icons/like-filled-icon";
 import { useState } from "react";
 import LikeNotFilledIcon from "../../assets/icons/like-not-filled-icon";
 
-function ImageCard() {
-  const [like, setLike] = useState(true);
+type ImageCardProps = {
+  isEditing: boolean;
+}
+
+function ImageCard({ isEditing }: ImageCardProps) {
+  const [like, setLike] = useState(false);
+  
   return (
     <ImgBox $imageurl={DummyImg}>
-      <LikeBtn onClick={() => setLike(false)}>{like ? <LikeFilledIcon /> : <LikeNotFilledIcon />}</LikeBtn>
+      {!isEditing && (
+        <LikeBtn onClick={() => setLike(!like)}>
+          {like ? <LikeFilledIcon /> : <LikeNotFilledIcon />}
+        </LikeBtn>
+      )}
     </ImgBox>
   );
 }
 
 export default ImageCard;
-const ImgBox = styled.div<{ $imageurl: string }>`
-  ${tw`w-full [aspect-ratio: 3 / 4] relative rounded-[8px] `}
 
-  background-image: url(${(props) => props.$imageurl});
-  background-size: cover;
-  background-position: center;
+const ImgBox = styled.div<{ $imageurl: string }>`
+    ${tw`w-full [aspect-ratio: 3 / 4] relative rounded-[8px] `}
+
+    background-image: url(${(props) => props.$imageurl});
+    background-size: cover;
+    background-position: center;
 `;
 
 const LikeBtn = styled.button`
-  ${tw`absolute w-[30px] h-[30px] rounded-full bg-[white] flex items-center justify-center bottom-1.5 right-1.5`}
+    ${tw`absolute w-[30px] h-[30px] rounded-full bg-[white] flex items-center justify-center bottom-1.5 right-1.5`}
 `;
