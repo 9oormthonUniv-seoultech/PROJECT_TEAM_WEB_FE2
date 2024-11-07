be9414441fd7e907650811fd4b697bee3795ab50
jaeyo03 <toyazx@naver.com>
2024-11-07 22:31:04 +0900

    fix : 좋아요 기능 수정

--- src/pages/Album/index.tsx
+++ src/pages/Album/index.tsx
@@ -156,14 +156,18 @@ function Album() {
     }
   };
   
-  const likePhotos = async (albumId:number, accessToken:string) => {
+  const likePhotos = async (albumId: number, accessToken: string) => {
     try {
-      const res = await Post(`/api/v1/album/like/${albumId}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      if(res.status === 200){
+      const res = await Post(
+        `/api/v1/album/like/${albumId}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      if (res.status === 200) {
         console.log(res.data.payload);
       }
     } catch (error) {
